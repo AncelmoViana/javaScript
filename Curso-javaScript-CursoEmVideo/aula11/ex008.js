0e526bb0a406d69cfb9e3fa61e2c2bb0eaf1f09e
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-05-24 11:02:20 -0300

    teste

---
+++ Curso-javaScript-CursoEmVideo/aula11/ex008.js
@@ -0,0 +1,12 @@
+var vel = 40;
+console.log(`\nA velocidade do seu carro é ${vel} km/h \n\n`);
+
+if(vel > 60){
+    console.log("Voce ultrapassou a velocidade permitida. MULTADO.");
+}
+else{
+    console.log('Dirija sempre usando cinto de segurança');
+
+}
+
+
