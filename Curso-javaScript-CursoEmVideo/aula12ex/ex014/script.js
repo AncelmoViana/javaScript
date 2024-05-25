989db85a59fe385aedfdcea69113e08468a4199c
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-05-25 11:15:04 -0300

    atualizaçao exercicios 25-05-2024

--- Curso-javaScript-CursoEmVideo/aula12ex/ex014/script.js
+++ Curso-javaScript-CursoEmVideo/aula12ex/ex014/script.js
@@ -1,1 +1,28 @@
-alert('testando javascript');
+
+
+function carregar(){
+    var msg = window.document.getElementById('msg');
+    
+    var img = window.document.getElementById('imagem');
+
+    var data = new Date();
+    var hora = data.getHours();
+    hora = 13;
+
+    msg.innerHTML = `Agora são ${hora} horas`;
+    if (hora >= 0 && hora < 12){
+        img.src= "imagens/manha.jpg";
+
+
+    }
+    else if(hora >= 12 && hora <= 18){
+        img.src= "imagens/tarde.jpg";
+
+    }
+    else{
+        img.src= "imagens/noite.jpg";
+
+    }
+
+}
+
