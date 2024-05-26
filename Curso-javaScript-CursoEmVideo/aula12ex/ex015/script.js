726038a381b221cf98be54c9fd6f3afe45759e5e
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-05-26 18:13:40 -0300

    inicio do ex 2 de javascript

---
+++ Curso-javaScript-CursoEmVideo/aula12ex/ex015/script.js
@@ -0,0 +1,76 @@
+function verificar(){
+    var data = new Date();
+    var ano = data.getFullYear();
+    var fano = document.getElementById('txtano');
+    var res = document.getElementById('res');
+    
+    if(fano.value.length == 0 || Number(fano.value) > ano){
+        alert("erro")
+    }
+    else{
+        var fsex = document.getElementsByName('radsex');
+
+        var idade = ano - Number(fano.value);
+        
+
+        var genero = '';
+
+        var img = document.createElement('img');
+        img.setAttribute('id', 'foto')
+
+        
+
+
+        if(fsex[0].checked){
+            genero = 'Homem';
+            if (idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'imagens/HomemBebe.jpg')
+
+            }
+            else if(idade < 21){
+                //jovem
+                img.setAttribute('src', 'imagens/HomemJovem.jpg' );
+
+            }
+            else if(idade < 50){
+                //adulto
+                img.setAttribute('src', 'imagens/HomemAdulto.jpg');
+
+            }
+            else{
+                //idoso
+                img.setAttribute('src', 'imagens/HomemIdoso.jpg');
+
+            }
+        }
+        else if(fsex[1].checked){
+            genero = 'Mulher';
+            if (idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src','imagens/MulherBebe.jpg' );
+
+            }
+            else if(idade < 21){
+                //jovem
+                img.setAttribute('src', 'imagens/MulherJovem.jpg');
+
+            }
+            else if(idade < 50){
+                //adulto
+                img.setAttribute('src', 'imagens/MulherAdulta.jpg');
+
+
+            }
+            else{
+                //idoso
+                img.setAttribute('src', 'imagens/MulherIdosa.jpg');
+
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detecamos ${genero} com ${idade} anos.`;
+        res.appendChild(img);
+        
+    }
+}
