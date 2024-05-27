14b018ad8055777754d2a8e6c4ba3264746a3a5c
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-05-27 17:23:30 -0300

    inicio da aula sobre repetiçao while

---
+++ Curso-javaScript-CursoEmVideo/aula13/ambiente.js
@@ -0,0 +1,7 @@
+
+var n = 1; 
+while(n <=3 ){
+    
+    console.log( n + ' Tudo bem?');
+    n++
+}
---
+++ Curso-javaScript-CursoEmVideo/aula13/node_be710ee2f0bac.tmp.js
@@ -0,0 +1,7 @@
+
+var n = 1; 
+while(n <=30000000000000 ){
+    
+    console.log( n + ' Tudo bem?');
+    n++
+}
