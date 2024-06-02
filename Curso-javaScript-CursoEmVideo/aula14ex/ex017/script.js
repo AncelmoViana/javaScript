88ed79f6fa98b9e24c48c28e3946e5a7e7a00ed2
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-06-02 10:09:08 -0300

    fim exercicio 017 javascript

--- Curso-javaScript-CursoEmVideo/aula14ex/ex017/script.js
+++ Curso-javaScript-CursoEmVideo/aula14ex/ex017/script.js
@@ -1,1 +1,33 @@
-alert('testando javascript');
+
+
+function gerar(){
+    let txtnumero = document.getElementById('n1');
+    let texto = document.getElementById('texto');
+
+    let numero = Number(txtnumero.value)
+    let s = 0;
+
+   
+
+
+  
+
+    if(txtnumero.value.length == 0){
+        alert('Campo Vazio! Tente Novamente!')
+    }
+    else{
+        texto.innerHTML = '';
+        for(i=1; i <= 10; i++){
+
+            let item = document.createElement('option');
+            
+           
+            item.text = `${numero} X ${i} = ${numero * i} `;
+            texto.appendChild(item)
+            
+      
+
+        }
+    }
+
+}
