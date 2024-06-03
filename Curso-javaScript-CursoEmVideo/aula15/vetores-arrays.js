b1ab1249af35250518396358ef94668fa204e3a8
Ancelmo Viana Oliveira <vianaancelmo@gmail.com>
2024-06-03 10:31:52 -0300

    Create vetores-arrays.js

---
+++ Curso-javaScript-CursoEmVideo/aula15/vetores-arrays.js
@@ -0,0 +1,30 @@
+let objetos = ['casa', 'copo', 'prato', 'chinelo', 'fone de ouvido', 'tv'];
+
+
+/*
+num.push(5);
+num.sort();
+
+
+
+console.log(`Nosso vetor é o ${num}`);
+console.log(`O vetor tem ${num.length} posições`);
+
+objetos.push('camisa');
+objetos.push('alicate');
+objetos.push('avião');
+objetos.sort();
+
+
+for(i=0; i<objetos.length; i++){
+    console.log(`posição ${i} = ${objetos[i]}`);
+}
+//Maneira simplificada
+for(let i in objetos){
+    console.log(`posição ${i} = ${objetos[i]}`);
+
+}
+*/
+let pos = objetos.indexOf('prato');
+console.log(`posicao do ${pos}`);
+
